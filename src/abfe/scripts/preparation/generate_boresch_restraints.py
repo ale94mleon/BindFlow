5609244bb3fcca7bd2794f9d7f0fdb468a7122c2
ale94mleon <ale94mleon@gmail.com>
2023-04-25 18:53:21 +0200

    adding more CLIs

--- src/abfe/scripts/preparation/generate_boresch_restraints.py
+++ src/abfe/scripts/preparation/generate_boresch_restraints.py
@@ -7,29 +7,24 @@ import argparse
 import MDAnalysis as mda
 from MDRestraintsGenerator import search, restraints
 
-if __name__ == "__main__":
+def gen_boresch_restraints():
 
-    def parse_args():
-        parser = argparse.ArgumentParser()
-        parser.add_argument('--top', default='../npt_prod1/npt_prod1.tpr',
-                            help=('path to input structure topology file '
-                                  '(e.g. TPR, PRM7)'))
-        parser.add_argument('--trj', default='npt_prod1_center.xtc',
-                            help=('path to input trajectory file '
-                                  '(e.g. XTC, NC, TRJ)'))
-        parser.add_argument('--ligand_selection', default="resname LIG and not name H*",
-                            help='ligand selection string')
-        parser.add_argument('--host_selection', default="protein and name CA",
-                            help='host atom selection string')
-        parser.add_argument('--temperature', type=float, default=298.15,
-                            help='simulation temperature')
-        parser.add_argument('--outpath', default='./',
-                            help='output path for writing files')
-        args = parser.parse_args()
-        return args
-
-
-    args = parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--top', default='../npt_prod1/npt_prod1.tpr',
+                        help=('path to input structure topology file '
+                                '(e.g. TPR, PRM7)'))
+    parser.add_argument('--trj', default='npt_prod1_center.xtc',
+                        help=('path to input trajectory file '
+                                '(e.g. XTC, NC, TRJ)'))
+    parser.add_argument('--ligand_selection', default="resname LIG and not name H*",
+                        help='ligand selection string')
+    parser.add_argument('--host_selection', default="protein and name CA",
+                        help='host atom selection string')
+    parser.add_argument('--temperature', type=float, default=298.15,
+                        help='simulation temperature')
+    parser.add_argument('--outpath', default='./',
+                        help='output path for writing files')
+    args = parser.parse_args()
 
     u = mda.Universe(args.top, args.trj)
 
@@ -42,7 +37,7 @@ if __name__ == "__main__":
 
     for l_atoms in ligand_atoms:
         psearch = search.FindHostAtoms(u, l_atoms[0],
-                                       p_selection=args.host_selection)
+                                    p_selection=args.host_selection)
         psearch.run(verbose=True)
         atom_set.extend([(l_atoms, p) for p in psearch.host_atoms])
 
@@ -57,3 +52,6 @@ if __name__ == "__main__":
 
     with open(f'{args.outpath}/dG_off.dat', 'w') as writer:
         writer.write(f'{dG}')
+
+
+if __name__ == "__main__":...
