d36b463e9d2a1e45876c2bfebf7eeac91653a0d3
Alejandro Martinez Leon <ale94mleon@gmail.com>
2024-03-20 18:26:09 +0100

    Adding mmpbsa example

---
+++ examples/CyclophilinD/example_execution-mmpbsa.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+"""
+This script is an example execution running for membrane systems.
+"""
+import yaml
+import glob
+from bindflow import calculate_mmpbsa
+
+ligand_mols = glob.glob("inputs/ligands/*mol")
+
+with open("config.yml", "r") as c:
+    global_config = yaml.safe_load(c)
+
+calculate_mmpbsa(
+    protein='inputs/protein.pdb',
+    ligands=ligand_mols,
+    out_root_folder_path="mmpbsa",
+    cofactor='inputs/dummy_cofactor_30.mol',
+    membrane=None,
+    hmr_factor=2.5,
+    threads=12,
+    num_jobs=100000,
+    replicas=3,
+    submit=False,
+    global_config=global_config)
+
+
+# rm -r abfe/slurm_logs/* abfe/*/*/slurm_logs/* abfe/.snakemake/ abfe/*/*/.snakemake abfe/*/*/ligand/ abfe/*/*/complex/
