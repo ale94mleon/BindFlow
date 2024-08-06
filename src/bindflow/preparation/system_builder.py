7d23aa4a76e0d83313a58d5bd05f3a7abd66469c
Alejandro Martinez Leon <ale94mleon@gmail.com>
2024-08-06 08:40:01 +0200

    Fix environ set

--- src/bindflow/preparation/system_builder.py
+++ src/bindflow/preparation/system_builder.py
@@ -338,7 +338,7 @@ class MakeInputs:
         # Setting environmental variable for user custom force field:
         if custom_ff_path:
             self.custom_ff_path = Path(custom_ff_path).resolve()
-            os.environ["GMXLIB"] = self.custom_ff_path
+            os.environ["GMXLIB"] = str(self.custom_ff_path)
         else:
             self.custom_ff_path = None
 
