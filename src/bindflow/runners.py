3d185405322ac7604fb7ade8737936cb6752f6d4
Alejandro Martinez Leon <ale94mleon@gmail.com>
2024-08-09 14:47:52 +0200

    Clean message

--- src/bindflow/runners.py
+++ src/bindflow/runners.py
@@ -325,7 +325,7 @@ def calculate(
             samples = 20
 
     print("Prepare")
-    print(f"\tPreparing {calculation_type}-Approach file structure: {_global_config['out_approach_path']}")
+    print(f"\tPreparing {calculation_type}-Approach file structure: {out_root_folder_path}")
 
     if not _global_config["ligand_names"]:
         raise ValueError("No ligands found")
