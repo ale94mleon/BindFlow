30e694bcd7c5065f751dfbcb272549c4c0761f83
Alejandro Martinez Leon <ale94mleon@gmail.com>
2025-03-07 15:28:40 +0100

    Update metadata

--- docs/conf.py
+++ docs/conf.py
@@ -114,7 +114,7 @@ html_logo = "source/_static/BindFlow-logo.svg"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = [] #['_static']
+html_static_path = []  # ['_static']
 
 intersphinx_mapping = {'Python': ('https://docs.python.org/3/', None),
                        'numpy': ('https://numpy.org/doc/stable/', None),
